d5f4bf89417d61e7926993cb9bb0f4bb7ea6ed08
alarrabee <alexlarrabee10@gmail.com>
2024-05-27 22:09:49 -0500

    add inquiter prompt and switch statement starter code

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,26 +1,26 @@
 INSERT INTO department (name) 
 VALUES
-1('Customer Service'),
-2('Engineering'),
-3('Finance'),
-4('Human Resources'),
-5('Legal'),
-6('Sales');
+('Customer Service'),
+('Engineering'),
+('Finance'),
+('Human Resources'),
+('Legal'),
+('Sales');
 
 INSERT INTO role (title, salary, department_id)
 VALUES
-1('Customer Service Lead' 72000, 1),
-2('Customer Service Associate' 60000, 1),
-3('Lead Engineer', 185000, 2),
-4('Associate Engineer', 133000, 2),
-5('Finance Manager', 139000, 3),
-6('Accountant', 106000, 3),
-7('Human Resources Manager', 85000, 4),
-8('Recruiter', 69000, 4),
-9('Legal Team Lead', 170000, 5),
-10('Lawyer', 82000, 5),
-11('Sales Lead', 175000, 6),
-12('Sales Representative', 132000, 6);
+('Customer Service Lead', 72000, 1),
+('Customer Service Associate', 60000, 1),
+('Lead Engineer', 185000, 2),
+('Associate Engineer', 133000, 2),
+('Finance Manager', 139000, 3),
+('Accountant', 106000, 3),
+('Human Resources Manager', 85000, 4),
+('Recruiter', 69000, 4),
+('Legal Team Lead', 170000, 5),
+('Lawyer', 82000, 5),
+('Sales Lead', 175000, 6),
+('Sales Representative', 132000, 6);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const {Pool} = require("pg");
 
 
 const pool = new Pool({
-    user:'postgres',
-    password:'password',
+    user:'',
+    password:'',
     host:'localhost',
     database:'employees_db'
 },
@@ -15,5 +15,79 @@ console.log(`Connected to the employees_db database`)
 pool.connect();
 
 
+function promptUser(){
+    return inquirer.prompt([
+        {
+            type:'list',
+            name:'prompt',
+            message:'What would you like to do?',
+            choices:[
+                {
+                    message:'View all employees',
+                    name:''
+                },
+                {
+                    message:'Add employee',
+                    name:''
+                },
+                {
+                    message:'Update Employee Role',
+                    name:''
+                },
+                {
+                    message:'View All Roles',
+                    name:''
+                },
+                {
+                    message:'Add Role',
+                    name:''
+                },
+                {
+                    message:'View All Departments',
+                    name:''
+                },
+                {
+                    message:'Add Department',
+                    name:''
+                },
+                {
+                    message:'Quit',
+                    name:''
+                }
+            ]
+        }
+    ])
+};
+
+function init(){
+    promptUser()
+    .then((answers) => {
+        let selection = answers.selection;
+        switch(selection){
+            case 'View all employees':
+                break;
+            case 'Add employee':
+                break;
+            case 'Update Employee Role':
+                break;
+            case 'View All Roles':
+                break;
+            case 'Add Role':
+                break;
+            case 'View All Departments':
+                break;
+            case 'Add Department':
+                break;
+            case 'Quit':
+                break;
+        }
+    })
+
+}
 
 
+
+
+
+
+init();
